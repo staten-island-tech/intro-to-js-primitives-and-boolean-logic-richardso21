8dce5e0b352243087180070d747e34cbfa45c35d
richardso21 <richard.so21@sitechhs.com>
2020-03-25 10:22:53 -0400

    lesson

---
+++ app.js
@@ -0,0 +1,85 @@
+// // let string = 'I am a string';
+// // let sentence = 'Hello Angel ' + 'and Vicky';
+// // console.log(sentence)
+
+// let student = 'Sandra';
+// let student2 = 'Gia';
+// let courseName = 'AP CSP';
+// let teacherName = 'Whalen';
+// let sentence = student + ' and ' + student2 + ' are taking ' + courseName + ' with ' + teacherName
+
+// let betterSentence = `${student} and ${student2} are taking ${courseName} with ${teacherName} next year`
+
+// // NUMS AND INTS
+
+// let year = 2020;
+// let nextYear = year + 1;
+// let past = year / 2
+// let future = year * 2
+
+// // Booleans
+
+// let trueStatement = false;
+
+// // Null and Undefined
+
+// let richard;
+
+
+// // if <-> else
+
+// let age = 17;
+// let baosquared = 'Bao Bao';
+// console.log(baosquared === 'Bao Bao');
+// console.log(age == '17');
+
+// let age = 17;
+// if (age < 18) {
+//     console.log('You are a Child.');
+// } else if (age === 18) {
+//     console.log('Happy Birthday');
+// } else {
+//     console.log('You are over 18');
+// }
+
+// // green, red, yellow
+
+// let color = 'green';
+// if (color === 'green') {
+//     console.log('Go')
+// } else if (color === 'red') {
+//     console.log('Stop')
+// } else if (color === 'yellow') {
+//     console.log('Caution')
+// } else {
+//     console.log('wtf')
+// }
+
+// // coffee
+
+// let kristyIntolerant = false;
+// let baoBaoIntolerant = false;
+
+// if (kristyIntolerant === baoBaoIntolerant) {
+//     if (kristyIntolerant === true) {
+//         console.log('Get almond milk');
+//     } else {
+//         console.log('Get regular milk');
+//     }
+// } else {
+//     console.log('Get regular and almond milk');
+// }
+
+// cupcake
+
+let temp = 69;
+
+if (temp < 68) {
+    console.log('too cold');
+} else if (temp >= 68 && temp <= 72) {
+    console.log('perfect');
+} else if (temp >= 73 && temp <= 76) {
+    console.log('warm')
+} else {
+    console.log('too hot')
+}
